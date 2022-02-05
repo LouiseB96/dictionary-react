1ada72598ee26be0e902d2c6637a1b6a9f160eef
LouiseB96 <louisebond96@hotmail.com>
2022-02-05 10:54:32 +0000

    added footer links

--- src/App.js
+++ src/App.js
@@ -7,10 +7,35 @@ function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary defaultKeyword="sunset" />
+          <Dictionary defaultKeyword="code" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Louise Bond</small>
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://loving-montalcini-3926a8.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Louise Bond
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/LouiseB96/dictionary-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://keen-heyrovsky-cd28d1.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
